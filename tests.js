74fc953d74b00629d8484fa2f6fa760a8d6981be
Sean Killeen <SeanKilleen@gmail.com>
2016-03-13 22:48:14 -0400

    Additional test specs

--- tests.js
+++ tests.js
@@ -4,3 +4,30 @@ describe ("A dummy test", function(){
         expect(x).toBe(2);
     });
 });
+
+xdescribe("Page load", function(){
+   xit("Loads a kitten in the background");
+   
+   xdescribe("Default options", function(){
+      xit("Has a choice of kitten, puppy, or bunny");
+      xit("Has kitten selected by default");
+      xit("Has a choie of reward levels");
+      xit("Has a reward level of 100 words selected by default"); 
+   });
+   
+   xdescribe("With no writing previously saved", function(){
+      xdescribe("Because the browser doesn't support local storage", function(){
+          xit("Starts with a blank text input");
+      });
+      
+      xdescribe("Because the user just didn't enter any", function(){
+          xit("Starts with a blank text input");
+      });
+   });
+   
+   xdescribe("If writing was previously saved", function(){
+       xit("Retrieves writing from local storage");   
+   });
+});
+
+xdescribe("")
